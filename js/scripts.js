143c1d5ccbcde1242ece76706988b6aeba4c903a
Brandon Wright <brandonfullstack@gmail.com>
2023-06-13 20:25:32 -0400

    Convert Fahrenheit to Celsius

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,15 @@
 // business logic
-function convert(celsius) {
+function convertCF(celsius) {
   return celsius * 1.8 + 32;
+}
+
+function convertFC(fahrenheit) {
+  return fahrenheit -32 * 5/9;
 }
 
 // user interface logic 
 const celsius = prompt("Enter a temperature in Degrees Celsius:");
+const fahrenheit = prompt("Enter a temperature in Degrees Fahrenheit:");
 
-window.alert(convert(celsius) + "° Fahrenheit");
+window.alert(convertCF(celsius) + "° Fahrenheit" + "\n" + 
+convertFC(fahrenheit) + "° Celsius");
